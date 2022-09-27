90f5628e15b02ab2b3063a50b9921b354e22d9fc
SrMashida <flavio.pprado4@gmail.com>
2022-09-27 16:26:25 -0300

    adding cache module. used cache: Redis.

--- src/domain/use-cases/user/find-user-by-key.usecase.ts
+++ src/domain/use-cases/user/find-user-by-key.usecase.ts
@@ -1,20 +1,30 @@
 import { User } from '../../entities/user.entity';
 import { IUserRepository } from '../../repositories/user.repository';
 import { IExceptionService } from '../../interfaces/exceptions.interface';
+import { ICacheManager } from '../../interfaces/cache.interface';
 
 export class FindUserByKeyUseCase {
   constructor(
     private readonly repository: IUserRepository,
     private readonly exceptionService: IExceptionService,
+    private readonly cacheManager: ICacheManager,
   ) {}
 
   public async execute(key: string, value: string): Promise<User> {
+    const cachedUser = await this.cacheManager.getCachedObject<User>(
+      'userById',
+    );
+
+    if (cachedUser) return cachedUser;
+
     const user: User = await this.repository.findByKey(key, value);
 
     if (!user)
       this.exceptionService.throwNotFoundException({
         message: 'User not found',
       });
+
+    await this.cacheManager.setObjectInCache('userById', user);
 
     return user;
   }
